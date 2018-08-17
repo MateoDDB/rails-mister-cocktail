3117b67bd85c2edd38e769ae2511c80513b8b0ab
Mateo Ceron <mateo-ceron@hotmail.fr>
2018-08-17 12:03:13 +0200

    mister cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Cuba Libre")
+Cocktail.create(name: "Caipi")
